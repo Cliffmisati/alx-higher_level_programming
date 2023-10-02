301508616a405328aa5c5d67f7b389b9f7efb5b6
Cliffmisati <cliffemisati@gmail.com>
2023-10-02 11:30:54 +0000

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,22 @@
+st class
+"""
+
+
+class MyList(list):
+    """
+    Inherits from list class
+    """
+
+    def __init__(self):
+        """
+        Initialises the object
+        """
+
+        super().__init__()
+
+    def print_sorted(self):
+        """
+        Prints the list, but sorted (ascending sort)
+        """
+
+        print(sorted(self))
